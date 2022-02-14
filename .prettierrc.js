093e4a6f7430dc46b48da9a1e31ba30aed6003ec
Thiago Cruz <29850573+thiagocrux@users.noreply.github.com>
2022-02-14 15:49:15 -0300

    create a prettier config file

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+export default {
+  trailingComma: "es5",
+  printWidth: 100,
+  singleQuote: true,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  endOfLine: "lf",
+  arrowParens: "avoid",
+}
