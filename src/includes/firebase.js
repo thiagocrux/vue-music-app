eacc6a250831cedc5b461ed1dd28e3ebd236eb3c
Thiago Cruz <29850573+thiagocrux@users.noreply.github.com>
2022-02-23 19:41:16 -0300

    add and config firebase

---
+++ src/includes/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB8ZA7qONmFNRP04AZOa1msVyVmAs3kyw0',
+  authDomain: 'music-34163.firebaseapp.com',
+  projectId: 'music-34163',
+  storageBucket: 'music-34163.appspot.com',
+  appId: '1:70276538787:web:a9864485b4051dc1ba7a16',
+};
+
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
