9627ef506cfb6648a52e5df67fefab703352c11c
Thiago Cruz <29850573+thiagocrux@users.noreply.github.com>
2022-02-23 22:57:00 -0300

    create an action to register and save user data

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,35 @@
 import { createStore } from 'vuex';
+import { auth, usersCollection } from '@/includes/firebase';
 
 export default createStore({
   state: {
     authModalShow: false,
+    userLoggedIn: false,
   },
   mutations: {
     toggleAuthModal: (state) => {
       state.authModalShow = !state.authModalShow;
     },
+    toggleAuth(state) {
+      state.userLoggedIn = !state.userLoggedIn;
+    },
   },
   getters: {
+  },
+  actions: {
+    async register({ commit }, payload) {
+      await auth.createUserWithEmailAndPassword(
+        payload.email, payload.password,
+      );
+
+      await usersCollection.add({
+        name: payload.name,
+        email: payload.email,
+        age: payload.age,
+        country: payload.country,
+      });
+
+      commit('toggleAuth');
+    },
   },
 });
