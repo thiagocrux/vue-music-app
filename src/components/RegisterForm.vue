ce9982e7d7d1b74fdf9dc9561035055d5a2f6a39
Thiago Cruz <29850573+thiagocrux@users.noreply.github.com>
2022-02-23 22:57:25 -0300

    update the function with an action

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -180,8 +180,6 @@
 </template>
 
 <script>
-import { auth, usersCollection } from '@/includes/firebase';
-
 export default {
   name: 'RegisterForm',
   data() {
@@ -211,26 +209,8 @@ export default {
       this.reg_alert_variant = 'bg-blue-500';
       this.reg_alert_msg = 'Please wait! Your account is being created.';
 
-      let userCredentials = null;
-
       try {
-        userCredentials = await auth.createUserWithEmailAndPassword(
-          values.email, values.password,
-        );
-      } catch (error) {
-        this.reg_in_submission = false;
-        this.reg_alert_variant = 'bg-red-500';
-        this.reg_alert_msg = 'An unexpected error occured. Please try again later.';
-        return;
-      }
-
-      try {
-        await usersCollection.add({
-          name: values.name,
-          email: values.email,
-          age: values.age,
-          country: values.country,
-        });
+        await this.$store.dispatch('register', values);
       } catch (error) {
         this.reg_in_submission = false;
         this.reg_alert_variant = 'bg-red-500';
@@ -240,8 +220,6 @@ export default {
 
       this.reg_alert_variant = 'bg-green-500';
       this.reg_alert_msg = 'Success! Your account has been created.';
-
-      console.log(userCredentials);
     },
   },
 };
