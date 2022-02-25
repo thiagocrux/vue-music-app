c511133447b7edb20a8e308d68b90be274380f60
Thiago Cruz <29850573+thiagocrux@users.noreply.github.com>
2022-02-25 00:56:02 -0300

    create login and init login actions

--- src/store/index.js
+++ src/store/index.js
@@ -18,10 +18,12 @@ export default createStore({
   },
   actions: {
     async register({ commit }, payload) {
+      // Create a new user in firebase (adds only email and password)
       const userCred = await auth.createUserWithEmailAndPassword(
         payload.email, payload.password,
       );
 
+      // Store all the user data with exception of the password using the same uid as before
       await usersCollection.doc(userCred.user.uid).set({
         name: payload.name,
         email: payload.email,
@@ -29,11 +31,29 @@ export default createStore({
         country: payload.country,
       });
 
+      // Set the display name as the user name
       await userCred.user.updateProfile({
         displayName: payload.name,
       });
 
+      // Toggle the authentication state to true
       commit('toggleAuth');
+    },
+    async login({ commit }, payload) {
+      // Send the authentication request
+      await auth.signInWithEmailAndPassword(payload.email, payload.password);
+
+      // Toggle the authentication state to true
+      commit('toggleAuth');
+    },
+    init_login({ commit }) {
+      // Retrieve the current authentication status from firebase
+      const user = auth.currentUser;
+
+      // Check if the user is logged in based on the value retrieved
+      if (user) {
+        commit('toggleAuth');
+      }
     },
   },
 });
