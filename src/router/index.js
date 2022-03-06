45f25f30f22706676568ed5e19cf89ab1af2ef92
Thiago Cruz <29850573+thiagocrux@users.noreply.github.com>
2022-03-06 14:47:16 -0300

    add route guards

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '@/views/Home.vue';
 import About from '@/views/About.vue';
 import Manage from '@/views/Manage.vue';
+import store from '@/store';
 
 const routes = [
   {
@@ -16,8 +17,17 @@ const routes = [
   },
   {
     name: 'manage',
+    // alias: '/manage',
     path: '/manage-music',
+    meta: {
+      requiresAuth: true,
+    },
     component: Manage,
+    // Specific route guard implementation
+    beforeEnter: (to, from, next) => {
+      console.log('Manage Route Guard');
+      next();
+    },
   },
   {
     path: '/manage',
@@ -33,6 +43,21 @@ const router = createRouter({
   history: createWebHashHistory(process.env.BASE_URL),
   routes,
   linkExactActiveClass: 'text-yellow-500',
+});
+
+// Global guard implementation
+router.beforeEach((to, from, next) => {
+  // console.log(to.matched);
+  if (!to.matched.some((record) => record.meta.requiresAuth)) {
+    next();
+    return;
+  }
+
+  if (store.state.userLoggedIn) {
+    next();
+  } else {
+    next({ name: 'home' });
+  }
 });
 
 export default router;
